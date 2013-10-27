6cfb4da9d68384bb5574fdabe54f19c879d50c7b
syzspectroom <syzspectroom@gmail.com>
2013-10-27 03:36:15 +0300

    problems list and show

--- app/controllers/api/problems_controller.rb
+++ app/controllers/api/problems_controller.rb
@@ -1,7 +1,39 @@
 class Api::ProblemsController < Api::BaseController
 
- def index
-    respond_with Category.find(params[:category_id]).subcategories.find(params[:category_id])
+  def index
+
+    serialized_problems = []
+    response = format_response
+    Problem.where(subcategory_id:response[:subcategory][:id]).each do |problem|
+      serialized_problems << ProblemSerializer.new(problem).to_json
+    end
+    response[:subcategory][:problems] = serialized_problems
+    respond_with response
   end
 
+  def show
+    response = format_response
+    response[:subcategory][:problem] = ProblemSerializer.new(
+        Problem.where(subcategory_id:response[:subcategory][:id], id: params[:id]).first
+      ).to_json
+    respond_with response
+  end
+
+  protected
+    def format_response
+      category = Category.find(params[:category_id])
+      subcategory = category.subcategories.find(params[:subcategory_id])
+
+      response = {
+        id: category.id.to_s,
+        title: category.title,
+        image: category.image,
+        subcategory: {
+          id: subcategory.id.to_s,
+          title: subcategory.title,
+        }
+      }
+
+      response
+    end
 end
--- spec/controllers/api/problems_controller_spec.rb
+++ spec/controllers/api/problems_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
-
 describe Api::ProblemsController do
 
  context 'show all' do
     describe 'GET #index' do
       before do
         category = FactoryGirl.create(:category)
+        problem1 = FactoryGirl.create(:problem, subcategory_id: category.subcategories.first.id.to_s)
+        problem2 = FactoryGirl.create(:problem, subcategory_id: category.subcategories.first.id.to_s)
+
         get :index, category_id: category.id.to_s, subcategory_id: category.subcategories.first.id.to_s
       end
       let(:body) { JSON.parse(response.body)}
@@ -13,10 +15,24 @@ describe Api::ProblemsController do
         response.response_code.should == 200
       end
 
-      it "response object should be array" do
-        p body
-        body.kind_of?(Array).should be_true
+    end
+  end
+
+
+ context 'show one' do
+    describe 'GET #show' do
+      before do
+        category = FactoryGirl.create(:category)
+        problem1 = FactoryGirl.create(:problem, subcategory_id: category.subcategories.first.id.to_s)
+        problem2 = FactoryGirl.create(:problem, subcategory_id: category.subcategories.first.id.to_s)
+
+        get :show, category_id: category.id.to_s, subcategory_id: category.subcategories.first.id.to_s, id: problem1.id.to_s
       end
+      let(:body) { JSON.parse(response.body)}
+      it "returns http 200" do
+        response.response_code.should == 200
+      end
+
     end
   end
 
