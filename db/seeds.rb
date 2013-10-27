424b2da9c7fae4f60a938ba590e79fb951f70936
syzspectroom <syzspectroom@gmail.com>
2013-10-27 06:49:07 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cat1 = FactoryGirl.create(:category)
+cat2 = FactoryGirl.create(:category)
+cat3 = FactoryGirl.create(:category)
+
+problem1 = FactoryGirl.create(:problem, subcategory_id: cat1.subcategories.first.id.to_s)
+problem2 = FactoryGirl.create(:problem, subcategory_id: cat1.subcategories.first.id.to_s)
+
+
+problem3 = FactoryGirl.create(:problem, subcategory_id: cat1.subcategories.last.id.to_s)
+problem4 = FactoryGirl.create(:problem, subcategory_id: cat1.subcategories.last.id.to_s)
+problem41 = FactoryGirl.create(:problem, subcategory_id: cat1.subcategories.last.id.to_s)
+
+
+
+
+problem5 = FactoryGirl.create(:problem, subcategory_id: cat2.subcategories.first.id.to_s)
+problem6 = FactoryGirl.create(:problem, subcategory_id: cat2.subcategories.first.id.to_s)
+problem7 = FactoryGirl.create(:problem, subcategory_id: cat2.subcategories.first.id.to_s)
+problem8 = FactoryGirl.create(:problem, subcategory_id: cat2.subcategories.first.id.to_s)
+problem8 = FactoryGirl.create(:problem, subcategory_id: cat2.subcategories.last.id.to_s)
